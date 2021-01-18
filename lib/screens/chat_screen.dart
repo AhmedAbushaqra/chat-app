e4e07ba3e54a9ba8ce54bc5fc05708f3ee429ad6
A7Abushaqra <ahmedhossin1122@gmail.com>
2021-01-18 17:43:52 +0200

    upload chat app

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,12 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:flash_chat/constants.dart';
+import '../constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatefulWidget {
+  static const String id = "Chat_Screen";
+
   @override
   _ChatScreenState createState() => _ChatScreenState();
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final messagetextcontroller= TextEditingController();
+  final _firestore = Firestore.instance;
+  final _auth = FirebaseAuth.instance;
+  FirebaseUser LogginedInUser;
+  String messagetext;
+  DateTime mTime=DateTime.now();
+
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
+  void getCurrentUser()async{
+    try {
+      final user = await _auth.currentUser();
+      if (user != null) {
+        LogginedInUser = user;
+      }
+    }catch(e){
+      print(e);
+    }
+  }
+
+  void messagesstream()async{
+     await for(var snapshot in _firestore.collection('masseges').snapshots()) {
+       for (var message in snapshot.documents) {
+
+       }
+     }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +52,8 @@ class _ChatScreenState extends State<ChatScreen> {
           IconButton(
               icon: Icon(Icons.close),
               onPressed: () {
-                //Implement logout functionality
+                _auth.signOut();
+                Navigator.pop(context);
               }),
         ],
         title: Text('⚡️Chat'),
@@ -27,6 +64,38 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            StreamBuilder<QuerySnapshot>(
+                stream: _firestore.collection('masseges').snapshots(),
+               builder: (context,snapshot){
+                  if(!snapshot.hasData){
+                    return Center(
+                      child: CircularProgressIndicator(
+                       backgroundColor: Colors.lightBlueAccent,
+                      ),
+                    );
+                  }
+                    final messages = snapshot.data.documents.reversed;
+                    List<MessageBubble> messageWidgets=[];
+                    for(var message in messages){
+                     String messageText = message.data['text'];
+                     String messagesender=message.data['sender'];
+
+                     final CurrentUser=LogginedInUser.email;
+
+                     final messageWidget =
+                         MessageBubble(sender: messagesender,text: messageText, isMe: CurrentUser==messagesender,);
+                         messageWidgets.add(messageWidget);
+                    }
+                    return Expanded(
+                      child: ListView(
+                        reverse: true,
+                        padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+                      children: messageWidgets,
+                    ),
+                    );
+
+               },
+            ),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -34,15 +103,21 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: <Widget>[
                   Expanded(
                     child: TextField(
+                      controller: messagetextcontroller,
                       onChanged: (value) {
-                        //Do something with the user input.
+                        messagetext = value;
                       },
                       decoration: kMessageTextFieldDecoration,
                     ),
                   ),
                   FlatButton(
                     onPressed: () {
-                      //Implement send functionality.
+                      messagetextcontroller.clear();
+                      _firestore.collection('masseges').add({
+                        //'time': mTime,
+                        'text':messagetext ,
+                        'sender': LogginedInUser.email,
+                      });
                     },
                     child: Text(
                       'Send',
@@ -54,6 +129,53 @@ class _ChatScreenState extends State<ChatScreen> {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+
+class MessageBubble extends StatelessWidget {
+  MessageBubble({this.sender,this.text,this.isMe});
+  String sender;
+  String text;
+  bool isMe;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: isMe?CrossAxisAlignment.end:CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+              sender,
+              style: TextStyle(
+                color: Colors.black54,
+                fontSize: 10,
+              ),
+          ),
+          Material(
+            borderRadius:isMe? BorderRadius.only(topLeft: Radius.circular(30.0)
+                                            ,bottomLeft: Radius.circular(30.0)
+                                            ,bottomRight: Radius.circular(30.0))
+                              :BorderRadius.only(topRight: Radius.circular(30.0)
+                                                ,bottomLeft: Radius.circular(30.0)
+                                                ,bottomRight: Radius.circular(30.0)),
+            elevation: 5.0,
+            color: isMe==true?Colors.lightBlueAccent:Colors.white,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10.0,vertical: 20.0),
+              child: Text(
+                text,
+                style: TextStyle(
+                  fontSize: 15,
+                  color: isMe==true?Colors.white:Colors.black,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import './login_screen.dart';
+import './registration_screen.dart';
 
 class WelcomeScreen extends StatefulWidget {
+
+  static const String id = "Welcome_screen";
+
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
@@ -18,9 +23,12 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           children: <Widget>[
             Row(
               children: <Widget>[
-                Container(
-                  child: Image.asset('images/logo.png'),
-                  height: 60.0,
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    child: Image.asset('images/logo.png'),
+                    height: 60.0,
+                  ),
                 ),
                 Text(
                   'Flash Chat',
@@ -42,12 +50,13 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 borderRadius: BorderRadius.circular(30.0),
                 child: MaterialButton(
                   onPressed: () {
-                    //Go to login screen.
+                    Navigator.pushNamed(context, LoginScreen.id);
                   },
                   minWidth: 200.0,
                   height: 42.0,
                   child: Text(
                     'Log In',
+                    style: TextStyle(color: Colors.white),
                   ),
                 ),
               ),
@@ -60,12 +69,13 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 elevation: 5.0,
                 child: MaterialButton(
                   onPressed: () {
-                    //Go to registration screen.
+                    Navigator.pushNamed(context, RegistrationScreen.id);
                   },
                   minWidth: 200.0,
                   height: 42.0,
                   child: Text(
                     'Register',
+                    style: TextStyle(color: Colors.white),
                   ),
                 ),
               ),
